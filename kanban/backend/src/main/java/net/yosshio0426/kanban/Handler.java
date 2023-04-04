a0e3995ec2d8fe70acf715115af0e72c6a3c3b72
yosshio0426 <kuniko192002@gmail.com>
2023-04-04 14:28:05 +0900

    #2 request/responseをapi gatewayの形式にする

--- kanban/backend/src/main/java/net/yosshio0426/kanban/Handler.java
+++ kanban/backend/src/main/java/net/yosshio0426/kanban/Handler.java
@@ -2,12 +2,17 @@ package net.yosshio0426.kanban;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
 import java.util.Map;
 
-public class Handler implements RequestHandler<Map<String, String>, String> {
+public class Handler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
     @Override
-    public String handleRequest(Map<String, String> stringStringMap, Context context) {
-        return "Hello World";
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .build();
     }
 }
